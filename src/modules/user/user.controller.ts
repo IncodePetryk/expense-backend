a22a73b452a836bcb19374f01690f5389cd8f77f
Oleksandr Petryk <oleksandr.petryk@incode-it.com>
2023-02-06 10:15:46 +0200

    feat: improve user module swagger documentation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,14 +1,19 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { ApiBasicAuth, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+
 import { JwtProtectedRequest } from '@Module/auth/interfaces/protected-request.interface';
 import { JwtAuthGuard } from '@Module/auth/jwt-auth.guard';
+import { UserDto } from '@Module/user/dto/user.dto';
 import { UserService } from '@Module/user/user.service';
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { ApiBasicAuth, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @ApiOperation({ description: 'Get own user profile' })
+  @ApiOkResponse({ description: 'User received successfully', type: UserDto })
+  @ApiUnauthorizedResponse({ description: 'User unauthorized' })
   @ApiBasicAuth('Bearer')
   @UseGuards(JwtAuthGuard)
   @Get('')
