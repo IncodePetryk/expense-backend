35bb2e308b6c0a61cf128675f90072292a33b0f4
Oleksandr Petryk <124143265+PetrykIncode@users.noreply.github.com>
2023-02-15 10:55:33 +0200

    feat: auth and expense modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { ExpenseModule } from '@Module/expense/expense.module';
+import { UserModule } from '@Module/user/user.module';
+import { PrismaModule } from '@Shared/modules/prisma/prisma.module';
+import { AuthModule } from '@Src/modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    ExpenseModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+
+import { AppModule } from '@Src/app.module';
+import { getEnv } from '@Src/utils/env-variable.util';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = getEnv.number('PORT');
+
+  // Setup middleware
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  app.enableCors();
+  app.use(cookieParser());
+
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Expense')
+    .setDescription('Expense API documentation')
+    .setVersion('0.0.1')
+    .addSecurity('Bearer', {
+      type: 'http',
+      scheme: 'Bearer',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Run server
+  await app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+  });
 }
+
 bootstrap();
